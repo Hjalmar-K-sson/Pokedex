55fb514fdc80f529b14754b4a3b2c743baef612d
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-06 21:31:45 +0100

    Created boilerplate App component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { Component } from "react";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: [],
+      search: "",
+    };
+  }
+  render() {
+    return <div>TEST DIV</div>;
+  }
+}
+
+export default App;
