03cebbf38ae2f9f5142bbca953a4dcca984c0225
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-10 19:37:59 +0100

    defined SearchBar component output

--- src/components/SearchBar/SearchBar.component.jsx
+++ src/components/SearchBar/SearchBar.component.jsx
@@ -3,7 +3,13 @@ import "./SearchBar.style.sass";
 
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return (
+      <input
+        className={this.props.className}
+        placeholder={this.props.placeholder}
+        onChange={this.props.onChange}
+      />
+    );
   }
 }
 
