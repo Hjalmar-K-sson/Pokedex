acfe6b13d93c4a7e96d3606cde49446ed94b65fc
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-10 17:59:23 +0100

    created CardGrid component

---
+++ src/components/CardGrid/CardGrid.component.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import "./CardGrid.style.sass";
+import Card from "../Card/Card.component";
+
+class CardGrid extends Component {
+  render() {
+    return <Card />;
+  }
+}
+
+export default CardGrid;
