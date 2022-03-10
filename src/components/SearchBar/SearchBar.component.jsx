33e196c8331738ecaf547adf61febb6d3557f7c3
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-10 18:00:58 +0100

    created SearchBar component

---
+++ src/components/SearchBar/SearchBar.component.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./SearchBar.style.sass";
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
